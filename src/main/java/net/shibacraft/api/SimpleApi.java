b61aa45af4018c9cb146dd204286ef3f589241bf
Daniel Vázquez <danielvazquezmunoz@gmail.com>
2023-05-06 15:06:34 +0200

    fsdff

--- src/main/java/net/shibacraft/api/SimpleApi.java
+++ src/main/java/net/shibacraft/api/SimpleApi.java
@@ -5,8 +5,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 public final class SimpleApi {
 
-    @Inject
-    private SimpleCore simpleCore;
+    private static SimpleCore simpleCore;
 
     private static <T> T check(@Nullable T instance) {
         if (instance == null) {
@@ -15,12 +14,13 @@ public final class SimpleApi {
         return instance;
     }
 
-    public SimpleCore getSimpleCore() {
+    public static SimpleCore getSimpleCore() {
         return check(simpleCore);
     }
 
-    /*public void initialize(SimpleCore core) {
+    @Inject
+    private static void setSimpleCore(SimpleCore core) {
         simpleCore = core;
-    }*/
+    }
 
 }
